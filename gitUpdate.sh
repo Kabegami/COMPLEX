af986141663a79f05c3b434f8b4d45a1dd30525d
Kabegami <lucas.becirspahic@gmail.com>
2017-10-18 17:47:46 +0200

    2.3

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,7 +1,7 @@
 #script pour incrementer la version car j'ai la fleme de le faire à la main
 X=`cat README.txt`
 git add .
-git commit -m "'$X'"
+git commit -m "$X"
 num=$(python -c "print $X + 0.1")
 echo $num > README.txt
 git push origin master
