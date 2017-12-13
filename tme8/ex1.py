21db24fe92afe3409b8255a2d2b5a2427b096577
Kabegami <lucas.becirspahic@gmail.com>
2017-12-13 12:48:47 +0100

    bla

--- tme8/ex1.py
+++ tme8/ex1.py
@@ -2,7 +2,14 @@
 
 import random
 import numpy as np
-import pdb
+import matplotlib.pyplot as plt
+import time
+
+def timeIt(f, *args):
+    t1 = time.time()
+    f(*args)
+    t2 = time.time()
+    return t2 - t1
 
 def diago(A, i,j):
     n,m = A.shape
@@ -24,11 +31,29 @@ def diago(A, i,j):
         j += 1
     return d
 
+def compatible_lines(A, j, n):
+    L = []
+    for i in range(n):
+        if not c(A,i,j):
+            L.append(i)
+    return L
+    
+
 def randomBacktrack(A):
     n,m = A.shape
-    while j < n:
-        #tirer une colone compatible
-        r = random.randint(
+    j = 0
+    while j < m:
+        L = compatible_lines(A, j, n)
+        #print('L : ', L)
+        if L == []:
+            j = 0
+            A = np.zeros((n,m))
+            #print('grille infaisable')
+            continue
+        r = random.choice(L)
+        A[r][j] = 1
+        j += 1
+    return A
         
 
 def backtrack2(A):
@@ -45,25 +70,39 @@ def backtrack2(A):
                 for l in range(0, m):
                     A[l][j] = 0
             if k not in dejaVu:
-                print('evaluation de k : ', k)
+                #print('evaluation de k : ', k)
                 A[k][j] = 1
                 if not conflit(A,k,j):
-                    print('pose block en i : {}, j : {}'.format(k,j))
+                    #print('pose block en i : {}, j : {}'.format(k,j))
                     b = True
                     dejaVu.append(k)
                     break
                 A[k][j] = 0
         last[j] = dejaVu
-        print('last[{}] : {}'.format(j, dejaVu))
+        #print('last[{}] : {}'.format(j, dejaVu))
         #pas de case possible backtrack
         if not b:
-            print('backtrack : ', j)
+            #print('backtrack : ', j)
             last[j] = []
             j -= 1
         else:
-            print('avance')
+            #print('avance')
             j +=1
     return A
+
+def c(A, i,j):
+    if sum(A[i]) >= 1:
+        return True
+    if sum(A[:, j]) >= 1:
+        return True
+    diag1 = np.diag(A, j - i)
+    #diag2 = np.diag(np.rot90(A),i)
+    diag2 = diago(A, i, j)
+    if sum(diag1) >= 1:
+        return True
+    if sum(diag2) >= 1:
+        return True
+    return False
             
 
 def conflit(A, i,j):
@@ -72,18 +111,30 @@ def conflit(A, i,j):
     if sum(A[:, j]) > 1:
         return True
     diag1 = np.diag(A, j - i)
-#    print('diag1 : ', diag1)
-    diag2 = np.diag(np.rot90(A),i)
-    #if j == 3: pdb.set_trace()
+    #diag2 = np.diag(np.rot90(A),i)
     diag2 = diago(A, i, j)
-    #print('diag2 : ', diag2)
-    #print('i : ', i)
-    #print('j : ', j)
     if sum(diag1) > 1:
         return True
     if sum(diag2) > 1:
         return True
     return False
+
+def evaluate(start=4, end=10):
+    L1 = []
+    L2 = []
+    nbC = []
+    for i in range(start, end+1):
+        M = np.zeros((i,i))
+        L1.append(timeIt(backtrack2, M))
+        L2.append(timeIt(randomBacktrack, M))
+        nbC.append(i)
+        print(".", end="", flush=True)
+    plt.plot(nbC, L1, label='iteratif')
+    plt.plot(nbC, L2, label='random')
+    plt.legend()
+    plt.show()
+        
+    
                    
 n = 8
 A = np.array([[0,0,0],
@@ -94,7 +145,9 @@ print('d : ', d)
 b = conflit(A, 0,2)
 print('b : ', b)
 M = np.zeros((n,n))
-M = backtrack2(M)
-print(M)
+#M = backtrack2(M)
+A = randomBacktrack(M)
+print(A)
+evaluate(end=20)
 
 
---
+++ tme8/ex2.py
@@ -0,0 +1,92 @@
+import random
+import math
+
+def pgcd(a,b):
+    if a < b:
+        return pgcd(b,a)
+    r = a % b
+    if r == 0:
+        return b
+    return pgcd(b,r)
+
+def myInverse(a,N):
+    for b in range(N):
+        temp = (a*b) 
+        if temp % N == 1:
+            return b
+    print("il n'existe pas d'inverse ! ")
+    return None
+
+def base2(n):
+    """ base 10 -> base 2"""
+    L = []
+    r = float('inf')
+    q = n
+    while q > 0:
+        r = q % 2
+        L.append(r)
+        q = q // 2
+    L.reverse()
+    return L
+
+def expoMod(n,g,N):
+    a = base2(n)
+    #print('a : ', a)
+    h = 1
+    for i in range(len(a)-1,-1,-1):
+        #print('i :',i)
+        h = (h*h) % N
+        if a[i] == 1:
+            h = (h * g) % N
+    return h
+
+def testFermat(p, maxi=20):
+    """ random """
+    cpt = 0
+    while cpt < maxi:
+        a = random.randint(1, p-1)
+        #b = expoMod(p-1, a,p)
+        #print('a: ', a)
+        #print('b: ',b)
+        b = a**(p-1) % p
+        #print('b : ',b)
+        if b != 1:
+            return False
+        cpt += 1
+    return True
+
+
+def firstTest(N):
+    for i in range(1,int(math.sqrt(N))):
+        if N % i == 0:
+            return False
+    return True
+
+def accuracy(start=1, end=10000, n=20):
+    cpt = 0
+    for i in range(n):
+        r = random.randint(start, end)
+        if testFermat(r) == firstTest(r):
+            cpt += 1
+    return cpt / float(n)
+    
+    
+def test():
+    assert(pgcd(2,3) == 1)
+    assert(pgcd(4,2) == 2)
+    assert(pgcd(9,6) == 3)
+    
+
+if __name__ == "__main__":
+    test()
+    b = myInverse(2,5)
+    print('b : ', b)
+    n = base2(24)
+    print('n : ', n)
+    p = expoMod(2,5,10)
+    print('p : ', p)
+    premier = testFermat(5)
+    print('test premier :', premier)
+    a = accuracy()
+    print('accuracy : ', a)
+    
